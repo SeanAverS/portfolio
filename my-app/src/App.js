0b5fb178b47ed4a36d33638cb9c232e513cbeac9
SeanAverS <seanasuguitan@gmail.com>
2024-11-23 12:28:09 -0800

    Initial page links setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Portfolio</h1>
+        <div className="column">
+          <h2>Projects</h2>
+          <ul>
+            <li><a href="#link1">Link 1</a></li>
+            <li><a href="#link2">Link 2</a></li>
+            <li><a href="#link3">Link 3</a></li>
+          </ul>
+        </div>
       </header>
     </div>
   );
